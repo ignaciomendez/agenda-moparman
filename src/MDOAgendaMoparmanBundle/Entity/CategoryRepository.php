e9b58ea22fb4825a1f1a161d880cdec280ea2808
Sergio Milardovich <sergio.milardovich@gmail.com>
2015-03-11 00:25:23 -0300

    Categories are now order alphabetically

--- src/MDOAgendaMoparmanBundle/Entity/CategoryRepository.php
+++ src/MDOAgendaMoparmanBundle/Entity/CategoryRepository.php
@@ -12,4 +12,8 @@ use Doctrine\ORM\EntityRepository;
  */
 class CategoryRepository extends EntityRepository
 {
+    public function findAll()
+    {
+        return $this->findBy(array(), array('name' => 'ASC'));
+    }
 }
